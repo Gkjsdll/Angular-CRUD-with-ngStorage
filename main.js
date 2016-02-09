cdfad71f79d306b283c614c38500e7036f923490
Zack Winchell <zackwinchell@gmail.com>
2016-02-08 21:49:43 -0800

    contacts are now linked to the view

--- main.js
+++ main.js
@@ -3,5 +3,18 @@
 var app = angular.module("angleCRUD", ["ngStorage"]);
 
 app.controller("angleCRUDctrl", function($scope) {
-  alert("controller loaded");
+
+  $scope.contacts = [];
+
+  $scope.contacts.push({
+    name: "John Doe",
+    email: "joedoe@bro.co",
+    phone: "867-5309"
+  });
+
+  $scope.contacts.push({
+    name: "Jane Doe",
+    email: "jaeday@hurray.na",
+    phone: "867-5309"
+  });
 });
