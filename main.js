ae30c3bd77883f620b3ad2bf273de6d7d535f663
Zack Winchell <zackwinchell@gmail.com>
2016-02-09 00:30:06 -0800

    contacts can be updated

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 
 var app = angular.module("angleCRUD", ["ngStorage"]);
 
+var $updateContact;
+
+$($updateContact = $("#updateContact"));
+
 app.controller("angleCRUDctrl", function($scope, $localStorage) {
 
   if(!$localStorage.contacts) $localStorage.contacts = [];
@@ -10,13 +14,46 @@ app.controller("angleCRUDctrl", function($scope, $localStorage) {
 
   $scope.contacts = $localStorage.contacts;
 
+  $scope.newFieldValue = "";
+
   $scope.addContact = function() {
-    $localStorage.contacts.push($scope.newContact)
-    $scope.newContact = null;
+    if($scope.newContact){
+      var contact = $scope.newContact;
+      if(contact.name && contact.email && contact.phone){
+        $localStorage.contacts.push($scope.newContact)
+        $scope.newContact = null;
+      }
+      else {
+        swal("Error", "You must enter a name, e-mail, and phone number.", "error");
+      }
+    } else {
+      swal("Error", "You must enter a name, e-mail, and phone number.", "error");
+    }
+
   }
 
   $scope.removeContact = function() {
     var index = $localStorage.contacts.indexOf(this.contact);
     $localStorage.contacts.splice(index, 1);
+  }
+
+
+  // var editing;
+
+  $scope.editContact = function($index) {
+    var editing = this;
+    var index = $localStorage.contacts.indexOf(this.contact);
+    var field = this.field.substr(0, 1).toUpperCase() + this.field.substr(1);
+    $("#modEditContact").modal();
+    $scope.modalEditField = field;
+    $scope.modalEditValue = this.value;
+    $updateContact.on("click", function(target) {
+      debugger;
+      $localStorage.contacts[index][editing.field] = $scope.newFieldValue;
+      $scope.contacts[index][editing.field] = $scope.newFieldValue;
+      $scope.newFieldValue = null;
+      $updateContact.off("click");
+      $("#modEditContact").modal('toggle');
+    });
   }
 });
